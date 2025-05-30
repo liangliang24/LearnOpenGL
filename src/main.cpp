d8e81f00340397c7b377827940e2595c1db9d356
LLTouchingFish <62233176+liangliang24@users.noreply.github.com>
2025-05-30 18:16:15 +0800

    render triangle

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,12 @@
 #include "backends/imgui_impl_glfw.h"
 #include "backends/imgui_impl_opengl3.h"
 
+#include <iostream>
+#include <fstream>
+
+#define WINDOW_WIDTH 1920
+#define WINDOW_HEIGHT 1080
+
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
     glViewport(0, 0, width, height);
@@ -18,6 +24,26 @@ void processInput(GLFWwindow* window)
         spdlog::info("press key escape");
 }
 
+std::string ReadFileAsString(const std::string& filepath)
+{
+    std::string result;
+    std::ifstream in(filepath, std::ios::in | std::ios::binary);
+    if (in)
+    {
+        in.seekg(0, std::ios::end);
+        result.resize((size_t)in.tellg());
+        in.seekg(0, std::ios::beg);
+        in.read(&result[0], result.size());
+        in.close();
+    }
+    else
+    {
+        spdlog::error("Could not open file '{0}'", filepath);
+    }
+
+    return result;
+}
+
 int main()
 {
     glfwInit();
@@ -26,7 +52,7 @@ int main()
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 
-    GLFWwindow* window = glfwCreateWindow(1920, 1080, "LearningOpenGL", NULL, NULL);
+    GLFWwindow* window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "LearningOpenGL", NULL, NULL);
     if (window == NULL)
     {
         spdlog::error("Failed to create GLFW window");
@@ -43,7 +69,7 @@ int main()
     }
     spdlog::info("Initialize GLAD successfully");
 
-    glViewport(0, 0, 800, 600);
+    glViewport(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT);
 
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
@@ -56,7 +82,108 @@ int main()
     ImGui_ImplGlfw_InitForOpenGL(window, true);
     ImGui_ImplOpenGL3_Init("#version 130");
 
+    int nrAttributes;
+    glGetIntegerv(GL_MAX_VERTEX_ATTRIBS, &nrAttributes);
+    spdlog::info("Maximum nr of vertex attributes supported: {0}", nrAttributes);
+
     ImVec4 clear_color = ImVec4(0.2f, 0.3f, 0.3f, 1.0f);
+    ImVec4 triangle_color = ImVec4(0.7f, 0.1f, 0.1f, 1.0f);
+
+    std::string vertexShaderSource = ReadFileAsString("assets/shaders/VertexShader.glsl");
+    unsigned int vertexShader;
+    vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    
+    const GLchar* vertexShaderSourceStr = vertexShaderSource.c_str();
+    glShaderSource(vertexShader, 1, &vertexShaderSourceStr, nullptr);
+    glCompileShader(vertexShader);
+
+    int success;
+    char infoLog[512];
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+    if (!success)
+    {
+        glGetShaderInfoLog(vertexShader, 512, nullptr, infoLog);
+        spdlog::error("VertexShader compile FAILED!{0}", infoLog);
+    }
+    else
+    {
+        spdlog::info("VertexShader compile SUCCESS!");
+    }
+
+    std::string fragmentShaderSource = ReadFileAsString("assets/shaders/FragmentShader.glsl");
+    unsigned int fragmentShader;
+    fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+
+    const GLchar* fragmentShaderSourceStr = fragmentShaderSource.c_str();
+    glShaderSource(fragmentShader, 1, &fragmentShaderSourceStr, nullptr);
+    glCompileShader(fragmentShader);
+
+    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+    if (!success)
+    {
+        glGetShaderInfoLog(fragmentShader, 512, nullptr, infoLog);
+        spdlog::error("FragmentShader compile FAILED!{0}", infoLog);
+    }
+    else
+    {
+        spdlog::info("FragmentShader compile SUCCESS!");
+    }
+
+    unsigned int shaderProgram;
+    shaderProgram = glCreateProgram();
+
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+
+    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+    if (!success) {
+        glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+        spdlog::error("ShaderProgram Link FAILED!{0}", infoLog);
+    }
+    else
+    {
+        spdlog::info("ShaderProgram Link Success!");
+    }
+
+    int uniColorLocation = glGetUniformLocation(shaderProgram, "uniColor");
+    int isUniformSetColorLocation = glGetUniformLocation(shaderProgram, "isUniformSetColor");
+
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+    float vertices[] =
+    {
+        -0.5f,  -0.5f,  0.0f,
+        0.5f,   -0.5f,  0.0f,
+        0.5f,   0.5f,   0.0f,
+        -0.5f,  0.5f,   0.0f
+    };
+
+    unsigned int indices[] =
+    {
+        0,  1,  3,
+        1,  2,  3
+    };
+
+    unsigned int VBO, VAO, EBO;
+    glGenBuffers(1, &VBO);
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &EBO);
+    glBindVertexArray(VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    glBindVertexArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 
     while (!glfwWindowShouldClose(window))
     {
@@ -69,9 +196,14 @@ int main()
         {
             ImGui::Begin("Hello World!");
 
-            if (ImGui::ColorEdit3("clear color", (float*)&clear_color))
+            if (ImGui::ColorEdit3("background color", (float*)&clear_color))
             {
-                spdlog::info("clear_color:{0}, {1}, {2}", clear_color.x, clear_color.y, clear_color.z);
+                spdlog::info("set background color:{0}, {1}, {2}", clear_color.x, clear_color.y, clear_color.z);
+            }
+
+            if (ImGui::ColorEdit4("triangle color", (float*)&triangle_color))
+            {
+                spdlog::info("set triangle color:{0}, {1}, {2}, {3}", triangle_color.x, triangle_color.y, triangle_color.z, triangle_color.w);
             }
 
             ImGui::End();
@@ -82,6 +214,12 @@ int main()
         glClearColor(clear_color.x, clear_color.y, clear_color.z, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
+        glUseProgram(shaderProgram);
+        glUniform4f(uniColorLocation, triangle_color.x, triangle_color.y, triangle_color.z, triangle_color.w);
+        glBindVertexArray(VAO);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        glBindVertexArray(0);
+
         ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 
         glfwSwapBuffers(window);
@@ -91,6 +229,10 @@ int main()
     ImGui_ImplOpenGL3_Shutdown();
     ImGui_ImplGlfw_Shutdown();
     ImGui::DestroyContext();
+
+    glDeleteBuffers(1, &EBO);
+    glDeleteBuffers(1, &VBO);
+    glDeleteVertexArrays(1, &VAO);
 
     glfwTerminate();
 	return 0;
